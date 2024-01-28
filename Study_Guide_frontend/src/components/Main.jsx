1b9367712f44498c370191e3e398e62c78f04ebd
Sahil <sohil170246@gmail.com>
2024-01-28 12:03:42 +0530

    frontend completed

--- Study_Guide_frontend/src/components/Main.jsx
+++ Study_Guide_frontend/src/components/Main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import data from "../assets/data.json"
+import Video from './video';
 
 const Main = () => {
-    console.log(data);
+    // console.log(data);
   return (
-    <div>Main</div>
+    <div className='main_container'>
+      {data.map((item, index)=> {
+        return (<div className='container'>
+          <h1 key={index}>{item.title}</h1>
+          <p>{item.desc}</p>
+          <Video embedId={item.url}></Video>
+        </div>);
+      })}
+    </div>
   )
 }
 
